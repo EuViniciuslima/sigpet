a9dba8cfb20c2f7f6fc7904f36303913714d3ce3
karol <anna.oliveira.700@ufrn.edu.br>
2021-10-21 11:28:50 -0300

    Etapa 1/15 - Criação da Tela Inicial e Tela Sobre

---
+++ SIG-Pet.c
@@ -0,0 +1,78 @@
+///////////////////////////////////////////////////////////////////////////////
+///             Universidade Federal do Rio Grande do Norte                 ///
+///                 Centro de Ensino Superior do Seridó                     ///
+///               Departamento de Computação e Tecnologia                   ///
+///                  Disciplina DCT1106 -- Programação                      ///
+///           Projeto Sistema de Agendamento de Consultas para Pets         ///
+///                Developed by  @OliveiraAnna99 - Out, 2021                ///
+///                Developed by  @EuViniciuslima - Out, 2021                ///
+///////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+
+
+void telaInicial(void);
+void telaSobre(void);
+void telaEquipe(void);
+
+
+int main(void){
+    telaInicial();
+    telaSobre();
+   
+}
+
+
+void telaInicial(void){
+    printf("                                                                          - □ x\n");
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
+    printf("///               Departamento de Computação e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
+    printf("///        Projeto Sistema de Agendamento de Consultas para Pets            ///\n");
+    printf("///                Developed by  @OliveiraAnna99 - Out, 2021                ///\n");
+    printf("///                Developed by  @EuViniciuslima - Out, 2021                ///\n"); 
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///    = = = = = Sistema de Agendamento de Consultas para Pets = = = = =    ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            1. Módulo Pet                                                ///\n");
+    printf("///            2. Módulo Consulta                                           ///\n");
+    printf("///            3. Módulo Usuário                                            ///\n");
+    printf("///            4. Módulo Avisos                                             ///\n");
+    printf("///            0. Sair                                                      ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
+void telaSobre(void){
+  
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
+    printf("///               Departamento de Computação e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
+    printf("///          Projeto Sistema de Agendamento de Consultas para Pets          ///\n");
+    printf("///                Developed by  @AnnaOliveira99 - Out, 2021                ///\n");
+    printf("///                Developed by  @EuViniciuslima - Out, 2021                ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///    = = = = = Sistema de Agendamento de Consultas para Pets = = = = =    ///\n");
+    printf("///                                                                         ///\n");
+    printf("///  O Programa SIG-Pet é ums sistema de agendamento de consultas para pets ///\n");
+    printf("///  desenvolvido para fins acadêmicos, de modo que o usuário seja capaz de ///\n");
+    printf("///  agendar uma consulta para seu animal de estimação do conforto da sua   ///\n");
+    printf("///  casa, além de acompanhar por meio do quadro de avisos, a data de vaci- ///\n");
+    printf("///  nação do seu bichinho, bem como as pendências do animal                ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
