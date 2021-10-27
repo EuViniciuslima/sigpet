af4b324802a178a02b6902c6d3eb3e1d7f99dd1e
karol <anna.oliveira.700@ufrn.edu.br>
2021-10-27 18:09:10 -0300

    Etapa 2/15 - Criação da Tela CRUD de Consulta,  Continuação do Sistema de Cadastro de Pet Usando Arquivos

--- SIG-Pet.c
+++ SIG-Pet.c
@@ -18,14 +18,17 @@ void telaMenu(void);
 void telaEquipe(void);
 void telaSobre(void);
 void telaPet(void);
+void telaConsulta(void);
 void telaCad_Pet(void);
+
+// Fazer login ou se cadastrar.
 
 int main(void)
 {
     int log_cadast;
     login_cadastro();
     printf("Digite a opção: \n1. Login\n2. Cadastrar-se\nEscolha: ");
-    scanf("%i", &log_cadast);
+    scanf("%d", &log_cadast);
 
     if (log_cadast == 1)
     {
@@ -102,27 +105,29 @@ void login(void)
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("\n");
 
-    
-    printf("Digite o seu email: " );
+    printf("Digite o seu email: ");
     char email_log[60];
     scanf("%s", email_log);
 
-    printf("Digite o sua senha: " );
+    printf("Digite o sua senha: ");
     char senha_log[60];
     scanf("%s", senha_log);
 
-    
     telaMenu();
-    
+
     printf("Escolha: ");
     int escolha;
     scanf("%d", &escolha);
 
-    if(escolha == 1){
+    if (escolha == 1)
+    {
         telaPet();
     }
-    
 
+    if (escolha ==2)
+    {
+        telaConsulta();
+    }
 }
 
 void cadastro(void)
@@ -153,7 +158,7 @@ void cadastro(void)
     printf("\n");
 
     FILE *pont_cadUsu;
-    pont_cadUsu = fopen("cadastro_usuario.txt", "w"); 
+    pont_cadUsu = fopen("cadastro_usuario.txt", "w");
     char email[60];
     printf("Digite o seu email: ");
     scanf("%s", email);
@@ -162,17 +167,17 @@ void cadastro(void)
     printf("Digite a sua senha: ");
     scanf("%s", senha);
 
-    fprintf(pont_cadUsu, "%s \n", email); 
-    fprintf(pont_cadUsu, "%s \n", senha); 
+    fprintf(pont_cadUsu, "%s \n", email);
+    fprintf(pont_cadUsu, "%s \n", senha);
     fclose(pont_cadUsu);
-
 
     printf("1. Login\n2. Sair\nEscolha:  ");
     int escolha;
     scanf("%d", &escolha);
 
-    if(escolha == 1){
-      login();
+    if (escolha == 1)
+    {
+        login();
     }
 }
 
@@ -202,9 +207,6 @@ void telaMenu(void)
     printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("\n");
-
-
-
 }
 void telaSobre(void)
 {
@@ -262,8 +264,9 @@ void telaEquipe(void)
     printf("///////////////////////////////////////////////////////////////////////////////\n");
 }
 
-
-void telaPet(void){
+// Tela de "cadastrar pet" está pronta. (disponível para alterações no código "linha 301")
+void telaPet(void)
+{
     printf("                                                                          - □ x\n");
     printf("\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
@@ -281,9 +284,9 @@ void telaPet(void){
     printf("///    = = = = = Sistema de Agendamento de Consultas para Pets = = = = =    ///\n");
     printf("///                                                                         ///\n");
     printf("///            1. Cadastrar Pet                                             ///\n");
-    printf("///            2. Pesquisar Pet                                     ///\n");
-    printf("///            3. Editar Pet                                         ///\n");
-    printf("///            4. Apagar Pet                                         ///\n");
+    printf("///            2. Pesquisar Pet                                             ///\n");
+    printf("///            3. Editar Pet                                                ///\n");
+    printf("///            4. Apagar Pet                                                ///\n");
     printf("///            0. Sair                                                      ///\n");
     printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
@@ -293,11 +296,16 @@ void telaPet(void){
     printf("Escolha: ");
     scanf("%d", &Escolha_Pet);
 
-    if(Escolha_Pet == 1){
+    if (Escolha_Pet == 1)
+    {
         telaCad_Pet();
     }
 }
-void telaCad_Pet(void){
+
+// telaCad_Pet sendo elaborada, mas até o momnento funcionando. Após o cadastro do animal ele irá voltar pra telaPet.
+
+void telaCad_Pet(void)
+{
 
     printf("                                                                          - □ x\n");
     printf("\n");
@@ -321,4 +329,74 @@ void telaCad_Pet(void){
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("\n");
 
+    FILE *cad_pet;
+
+    cad_pet = fopen("cadastro_pet.txt", "w");
+    char nome_pet[60];
+    printf("Digite o nome do animal:\t");
+    scanf("%s", nome_pet);
+
+    int idade_pet;
+    printf("Digite a idade do animal:\t");
+    scanf("%d", &idade_pet);
+
+    char sexo_pet[60];
+    printf("Digite o sexo do animal:\t");
+    scanf("%s", sexo_pet);
+
+    char especie_pet[60];
+    printf("Digite a especie do animal:\t");
+    scanf("%s", especie_pet);
+
+    char dono_pet[60];
+    printf("Digite o nome do dono do animal:\t");
+    scanf("%s", dono_pet);
+
+    fprintf(cad_pet, " %s\n", nome_pet);
+    fprintf(cad_pet, "%d\n", idade_pet);
+    fprintf(cad_pet, "%s\n", sexo_pet);
+    fprintf(cad_pet, "%s\n", especie_pet);
+    fprintf(cad_pet, "%s\n", dono_pet);
+    fclose(cad_pet);
+
+    int cad_perg;
+    printf("Deseja fazer um novo cadastro?\n1. Novo Cadastro\n2. Concluir Cadastro\nSua Escolha: ");
+    scanf("%d", &cad_perg);
+
+    if (cad_perg == 1)
+    {
+        telaCad_Pet();
+    }
+    if (cad_perg == 2)
+    {
+        telaPet();
+    }
+}
+
+void telaConsulta(void)
+{
+    printf("                                                                          - □ x\n");
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
+    printf("///               Departamento de Computação e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
+    printf("///        Projeto Sistema de Agendamento de Consultas para Pets            ///\n");
+    printf("///                Developed by  @OliveiraAnna99 - Out, 2021                ///\n");
+    printf("///                Developed by  @EuViniciuslima - Out, 2021                ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///    = = = = = Sistema de Agendamento de Consultas para Pets = = = = =    ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            1. Cadastrar Consulta                                        ///\n");
+    printf("///            2. Pesquisar Consulta                                        ///\n");
+    printf("///            3. Editar Consulta                                           ///\n");
+    printf("///            4. Apagar Consulta                                           ///\n");
+    printf("///            0. Sair                                                      ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
 }
